8989308c678d910e9e7350c2984f06288abd51d1
Kalyani <samantarayk2001@gmail.com>
2026-03-04 23:24:26 +0530

    chatBot Project

---
+++ chatbot-project/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react'
+import { Chatbot } from 'supersimpledev';
+import ChatInput from './components/ChatInput.jsx';
+import ChatMessages from './components/ChatMessages.jsx';
+import './App.css'    
+
+function App() {
+      const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('messages')) || [{
+          message: 'hello chatbot',
+          sender: 'user',
+          id: 'id1',
+          time:  1736127288920
+        }, {
+          message: 'Hello! How can I help you?',
+          sender: 'robot',
+          id: 'id2',
+          time: 1736127291230
+        }, {
+          message: 'can you get me todays date?',
+          sender: 'user',
+          id: 'id3',
+          time: 1736127385356
+        }, {
+          message: 'Today is September 27',
+          sender: 'robot',
+          id: 'id4',
+          time: 1736127385500
+        }]);
+       
+     // const [chatMessages, setChatMessages] = array;
+     // const chatMessages = array[0];
+     // const setChatMessages = array[1];
+
+    useEffect(() => {
+      Chatbot.addResponses({
+        'goodbye': 'Goodbye. Have a great day!',
+        'give me a unique id': function() {
+          return `Sure! Here's a unique ID: ${crypto.randomUUID()}`;
+      }
+    });
+
+    /* 
+    [] tells useEffect to only run once. We only want to run this setup  code once because we only wnt to add these extra responses once. 
+     */
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('message', JSON.stringify(chatMessages));
+  }, [chatMessages]);
+     
+      return(
+        <div className= "app-container">           
+          <ChatMessages 
+           chatMessages={chatMessages}
+          /> 
+          <ChatInput 
+           chatMessages={chatMessages}
+           setChatMessages={setChatMessages}
+          />        
+        </div>
+      );     
+    }
+
+export default App
