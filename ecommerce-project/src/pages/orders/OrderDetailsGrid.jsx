77e7446dd25025e32b66b22cf18f8eec334aeffc
Kalyani <samantarayk2001@gmail.com>
2026-03-27 19:16:31 +0530

    Moved OrderHeader and OrderDetailsGrid to their own components

---
+++ ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
@@ -0,0 +1,43 @@
+import dayjs from 'dayjs';
+import { Fragment } from 'react';
+import BuyAgainIcon from '../../assets/images/icons/buy-again.png';
+
+export function OrderDetailsGrid({ order }) {
+  return (
+    <div className="order-details-grid">
+      {order.products.map((orderProduct) => {
+        return (
+          <Fragment key={orderProduct.product.id}>
+            <div className="product-image-container">
+              <img src={orderProduct.product.image} />
+            </div>
+
+            <div className="product-details">
+              <div className="product-name">
+                {orderProduct.product.name}
+              </div>
+              <div className="product-delivery-date">
+                Arrining on: {dayjs(orderProduct.estimatedDeliveryTimeMs).format('MMMM D')}
+              </div>
+              <div className="product-quantity">
+                Quantity: {orderProduct.quantity}
+              </div>
+              <button className="buy-again-button button-primary">
+                <img className="buy-again-icon" src={BuyAgainIcon} />
+                <span className="buy-again-message">Add to Cart</span>
+              </button>
+            </div>
+
+            <div className="product-actions">
+              <a to="/tracking">
+                <button className="track-package-button button-secondary">
+                  Track package
+                </button>
+              </a>
+            </div>
+          </Fragment>
+        );
+      })}
+    </div>
+  );
+}
